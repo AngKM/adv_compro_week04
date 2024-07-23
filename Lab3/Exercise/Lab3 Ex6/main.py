50007b1267d7758a3c4ac93879aad10d567f56a6
LemonSauce <102318580+AngKM@users.noreply.github.com>
2024-07-23 17:08:49 +0700

    laast

--- Lab3/Exercise/Lab3 Ex6/main.py
+++ Lab3/Exercise/Lab3 Ex6/main.py
@@ -23,3 +23,12 @@
 # print("Capitalized:", so.capitalize_string(sample_string))
 # print("Lowercase:", so.lowercase_string(sample_string))
 # print("Uppercase:", so.uppercase_string(sample_string))
+import string_operations
+
+sample_string = "hello World"
+
+print("Original:", sample_string)
+print("Reversed:", string_operations.reverse_string(sample_string))
+print("Capitalized:", string_operations.capitalize_string(sample_string))
+print("Lowercase:", string_operations.lowercase_string(sample_string))
+print("Uppercase:", string_operations.uppercase_string(sample_string))
