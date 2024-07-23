4455e1af80e5ab2b10a0d7a92f11397a297d5770
LemonSauce <102318580+AngKM@users.noreply.github.com>
2024-07-23 16:42:01 +0700

    Withstringop

--- Lab3/Exercise/Lab3 Ex6/string_operations.py
+++ Lab3/Exercise/Lab3 Ex6/string_operations.py
@@ -3,4 +3,8 @@
 #     - `capitalize_string`: Capitalizes the first letter of a given string.
 #     - `lowercase_string`: Converts all characters in a given string to lowercase.
 #     - `uppercase_string`: Converts all characters in a given string to uppercase.
+reverse_string = lambda word: word[::-1]
+capitalize_string = lambda word: word.capitalize()
+lowercase_string = lambda word: word.lower()
+uppercase_string = lambda word: word.upper()
 
